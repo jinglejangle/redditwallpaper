732f35ead930f710caa2c3522153443ff91f3c60
mark <mark@jupiter.wandisco.com>
2014-10-21 14:49:57 +0100

    bugfix

--- redditWallpaper.php
+++ redditWallpaper.php
@@ -40,6 +40,7 @@ class redditWallpaper {
     public $subreddit = 'http://www.reddit.com/r/wallpaper'; 
     private $images = [];
     private $saveto = '/tmp';
+    private $fallback_image = '';
 
     function __construct($subreddits=array(), $saveto=''){ 
 
@@ -71,6 +72,7 @@ class redditWallpaper {
     function setWallpaper($paper) { 
         $paper = escapeshellcmd($paper);
         $cmd = str_replace("::FILE::", $paper, SET_BG_COMMAND ); 
+        echo "cmd: $cmd \n"; 
         exec($cmd);
     }
 
@@ -87,26 +89,39 @@ class redditWallpaper {
 
     }
 
-    function fetchWallpaper(){ 
-        $html = file_get_contents($this->subreddit); 
+    function getImages(){ 
+        
 	$this->images=array();
+        
+        $html = file_get_contents($this->subreddit); 
         $this->images = $this->extractImages($html); 
+        if(empty($this->images)){ 
+                $this->subreddits =  array_diff($this->subreddits, [$this->subreddit]);//remove offending subreddit and try again
+                if(count($this->subreddits)){ 
+                    $this->selectSubReddit($this->subreddits); 
+                    $this->images = $this->getImages(); 
+                }else{
+                        die("Tried all subreddits. something else is badly wrong. ");
+                }
+        }
+        return $this->images; 
 
-	if(empty($this->images)){ 
-		$this->selectSubReddit($this->subreddits); 
-        	$this->images = $this->extractImages($html); 
-	}
+    }
 
+    function fetchWallpaper(){ 
+        $this->images = $this->getImages(); 
+        if(empty($this->images)){ 
+                die("Can't find any images in $this->subreddit ");
+        }
         $image = false;
-
         $trycount=0; 
-        while(!$image && $trycount < 10){  
+        while(!$image && $trycount < 20){  
             $image = $this->fetchImage($this->images) ; 
             $trycount++; 
         }
-        if($trycount>8){ 
-                echo "I did a lot of tries, something was wrong with $this->subreddit image: $image\n";  
-                die();
+        if($trycount>18){ 
+                echo "I did a lot of tries, something was wrong with $this->subreddit image: $image. Using fallback image $this->fallback_image\n "; 
+               return $this->fallback_image;  
         }
         return $image; 
     }
@@ -121,11 +136,13 @@ class redditWallpaper {
         }
         shuffle($images);
         foreach($images as $url){ 
+                //echo "checking image $url\n";
             $filename = basename($url); 
 
             if($this->checkAlreadySeen($filename)){ 
-                //return $filename; 
-                break; 
+                $this->fallback_image =  AUTO_PICTURE_DIR."/".$filename; 
+                return AUTO_PICTURE_DIR."/".$filename;
+                //break; 
             }
     
             $ch = curl_init ($url);
